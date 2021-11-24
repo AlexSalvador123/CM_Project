60ab4e9dfd193e0ec0ea906a5c0906b8e2e5a59f
Alex <alex2000.salvador@gmail.com>
2021-11-24 17:40:02 +0000

    inicio da pagina de login

---
+++ TrackMySport/app/src/main/java/com/example/trackmysport/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.trackmysport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+    Handler h = new Handler();
+    Runnable r = new Runnable() {
+
+        @Override
+        public void run() {
+            // if you are redirecting from a fragment then use getActivity() as the context.
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+        };
+    };
+
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // The Runnable will be executed after the given delay time
+        h.postDelayed(r, 3000);
+    };
+
+    public void GoToScreen (View view){
+        h.removeCallbacks(r);
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    };
+};
