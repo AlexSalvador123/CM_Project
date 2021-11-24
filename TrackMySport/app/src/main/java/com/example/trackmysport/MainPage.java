97ec3dc74754ccabed0436116c4d9412b5a5d7fa
Alex <alex2000.salvador@gmail.com>
2021-11-24 22:14:28 +0000

    main page inicio

--- TrackMySport/app/src/main/java/com/example/trackmysport/MainPage.java
+++ TrackMySport/app/src/main/java/com/example/trackmysport/MainPage.java
@@ -6,17 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_page);
     }
 
-    public void GoToMainPage (View view){
-        Intent i = new Intent(this, MainPage.class);
-        startActivity(i);
-        finish();
-    };
 }
