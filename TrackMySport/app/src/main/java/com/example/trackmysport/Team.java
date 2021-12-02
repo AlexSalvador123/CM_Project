fb078622ce08e8abdbfe30d76ab2d7015733661c
Alex <alex2000.salvador@gmail.com>
2021-12-02 16:11:52 +0000

    teams

--- TrackMySport/app/src/main/java/com/example/trackmysport/Team.java
+++ TrackMySport/app/src/main/java/com/example/trackmysport/Team.java
@@ -7,25 +7,33 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Team extends AppCompatActivity {
 
     FloatingActionButton fragmentCreate;
+    Button teamCreated;
+    boolean frag= true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
         fragmentCreate = findViewById(R.id.floatingActionButton);
+
         replaceFragment(new manageTeamsFragment());
         fragmentCreate.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View view) {
-
-                  replaceFragment(new CreateTeamFragment());
-
+                  if (frag==true) {
+                      replaceFragment(new CreateTeamFragment());
+                      frag=false;
+                  }else{
+                      replaceFragment(new manageTeamsFragment());
+                      frag=true;
+                  }
               }
           }
         );
@@ -37,5 +45,11 @@ public class Team extends AppCompatActivity {
         fragmentTransaction.replace(R.id.fragment_container, fragment);
         fragmentTransaction.commit();
     }
+
+    public void createTeam(View view){
+        frag=true;
+        replaceFragment(new manageTeamsFragment());
+    }
+
 
 }
