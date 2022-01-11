0a4c8740ca29ee0c689862a749efb0e53319996e
Alex <alex2000.salvador@gmail.com>
2022-01-11 02:11:57 +0000

    adicionar evento e adicionar elemento 50/100 a uma equipa

--- TrackMySport/app/src/main/java/com/example/trackmysport/Team.java
+++ TrackMySport/app/src/main/java/com/example/trackmysport/Team.java
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -84,6 +86,61 @@ public class Team extends AppCompatActivity {
         String name = editText.getText().toString();
         dr.child("Teams").child(name).removeValue();
         replaceFragment(new manageTeamsFragment());
+    }
+
+    public void createEvent(View v){
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
+        final View popV = getLayoutInflater().inflate(R.layout.eventpop,null);
+        builder.setTitle("Event")
+                .setView(popV)
+                .setNegativeButton("cancel", null)
+                .setPositiveButton("done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        EditText edit = (EditText) popV.findViewById(R.id.editTextDate);
+                        Editable editable = edit.getText();
+                        String date = editable.toString();
+                        EditText edit1 = (EditText) popV.findViewById(R.id.editText);
+                        Editable editable1 = edit1.getText();
+                        String title = editable1.toString();
+                        EditText edit2 = (EditText) popV.findViewById(R.id.editTextPostalAddress);
+                        Editable editable2 = edit2.getText();
+                        String address = editable2.toString();
+                        fd = FirebaseDatabase.getInstance("https://trackmysport-ff56d-default-rtdb.europe-west1.firebasedatabase.app/");
+                        dr = fd.getReference();
+                        TextView editText = (TextView) findViewById(R.id.titleTeamName);
+                        String name = editText.getText().toString();
+                        dr.child("Teams").child(name).child("events").child("1").child("date").setValue(date);
+                        dr.child("Teams").child(name).child("events").child("1").child("title").setValue(title);
+                        dr.child("Teams").child(name).child("events").child("1").child("address").setValue(address);
+
+                    }
+                });
+        builder.show();
+    }
+
+    public void addElement(View v){
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
+        final View popE = getLayoutInflater().inflate(R.layout.addelementpop,null);
+        builder.setTitle("Event")
+                .setView(popE)
+                .setNegativeButton("cancel", null)
+                .setPositiveButton("done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        EditText edit = (EditText) popE.findViewById(R.id.editTextEmailAddress4);
+                        Editable editable = edit.getText();
+                        String email = editable.toString();
+
+                        /*fd = FirebaseDatabase.getInstance("https://trackmysport-ff56d-default-rtdb.europe-west1.firebasedatabase.app/");
+                        dr = fd.getReference();
+                        TextView editText = (TextView) findViewById(R.id.titleTeamName);
+                        String name = editText.getText().toString();
+                        dr.child("Teams").child(name).child("events").child("1").child("date").setValue(date);*/
+
+                    }
+                });
+        builder.show();
     }
 
 
