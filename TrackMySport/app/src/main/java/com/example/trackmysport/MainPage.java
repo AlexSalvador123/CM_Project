94d4ef049d7659bcfeafc8e66e334474a5c24aa4
Alex <alex2000.salvador@gmail.com>
2022-01-19 22:35:11 +0000

    mainpage

--- TrackMySport/app/src/main/java/com/example/trackmysport/MainPage.java
+++ TrackMySport/app/src/main/java/com/example/trackmysport/MainPage.java
@@ -12,49 +12,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPage extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-        bottomNavigationView = findViewById(R.id.bottomNav);
-        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
-        bottomNavigationView.setSelectedItemId(R.id.profile);
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new
-            BottomNavigationView.OnNavigationItemSelectedListener(){
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    System.out.println(item.getItemId());
-                    switch(item.getItemId()){
-                        case R.id.player:
-                            Intent i1 = new Intent(MainPage.this, TrainingSession.class);
-                            startActivity(i1);
-                            return true;
-                        case R.id.teams:
-                            Intent i2 = new Intent(MainPage.this, Team.class);
-                            startActivity(i2);
-                            return true;
-                        case R.id.profile:
-
-                            return true;
-                        case R.id.teach:
-                            Intent i3 = new Intent(MainPage.this, DrawActivities.class);
-                            startActivity(i3);
-                            return true;
-                        case R.id.agenda:
-                            Intent i4 = new Intent(MainPage.this, Agenda.class);
-                            startActivity(i4);
-                            return true;
-
-
-                    }
-                return false;
-                }
-            };
 
     public void Profile(View view){
         Intent i = new Intent(this, Profile.class);
@@ -66,18 +29,18 @@ public class MainPage extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void Videos(View view){
+    /*public void Videos(View view){
         Intent i = new Intent(this, Videos.class);
         startActivity(i);
-    }
+    }*/
 
     public void TrainingSession(View view){
         Intent i = new Intent(this, TrainingSession.class);
         startActivity(i);
     }
 
-    public void Whiteboard(View view){
-        Intent i = new Intent(this, Whiteboard.class);
+    public void Explicar(View view){
+        Intent i = new Intent(this, DrawActivities.class);
         startActivity(i);
     }
 
