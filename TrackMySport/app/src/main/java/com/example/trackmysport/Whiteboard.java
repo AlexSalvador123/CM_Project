eb08dc295173b471a05e23743a736d2859636752
danielserras <dfserras@gmail.com>
2022-01-19 17:13:45 +0000

    design

--- TrackMySport/app/src/main/java/com/example/trackmysport/Whiteboard.java
+++ TrackMySport/app/src/main/java/com/example/trackmysport/Whiteboard.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.R.drawable;
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -21,10 +22,12 @@ import android.graphics.drawable.RippleDrawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.GestureDetector;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.lang.reflect.Field;
@@ -38,6 +41,7 @@ public class Whiteboard extends AppCompatActivity {
     private FloatingActionButton delete;
     private FloatingActionButton save;
     private FloatingActionButton openPreferences;
+    private BottomNavigationView bottomNavigationView;
     Preferences preferences;
 
 
@@ -45,6 +49,9 @@ public class Whiteboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whiteboard);
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        bottomNavigationView.setSelectedItemId(R.id.teach);
         setTitle("Whiteboard");
         ConstraintLayout whiteboard = findViewById(R.id.whiteboard);
         paintCanvas = new PaintCanvas(Whiteboard.this, null);
@@ -117,4 +124,32 @@ public class Whiteboard extends AppCompatActivity {
         paintCanvas.changeStrokeColor(color);
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new
+            BottomNavigationView.OnNavigationItemSelectedListener(){
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch(item.getItemId()){
+                        case R.id.player:
+                            Intent i1 = new Intent(Whiteboard.this, TrainingSession.class);
+                            startActivity(i1);
+                            return true;
+                        case R.id.teams:
+                            Intent i2 = new Intent(Whiteboard.this, Team.class);
+                            startActivity(i2);
+                            return true;
+                        case R.id.teach:
+
+                            return true;
+                        case R.id.agenda:
+                            Intent i4 = new Intent(Whiteboard.this, Agenda.class);
+                            startActivity(i4);
+                            return true;
+                        case R.id.profile:
+                            Intent i5 = new Intent(Whiteboard.this, Profile.class);
+                            startActivity(i5);
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
