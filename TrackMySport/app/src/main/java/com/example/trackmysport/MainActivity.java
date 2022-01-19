a0d561d6b23f4f6f7f097c2fab855d3b0d0d83d7
Alex <alex2000.salvador@gmail.com>
2022-01-19 02:17:27 +0000

    mudar a pagina principal

--- TrackMySport/app/src/main/java/com/example/trackmysport/MainActivity.java
+++ TrackMySport/app/src/main/java/com/example/trackmysport/MainActivity.java
@@ -19,6 +19,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -75,6 +80,23 @@ public class MainActivity extends AppCompatActivity {
     }
     public void updateUI(FirebaseUser account){
         if(account != null){
+            FirebaseDatabase mDatabase = FirebaseDatabase.getInstance("https://trackmysport-ff56d-default-rtdb.europe-west1.firebasedatabase.app/");
+            DatabaseReference databaseReference = mDatabase.getReference();
+            databaseReference.child("Users").child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    System.out.println(snapshot);
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
             startActivity(new Intent(this,MainPage.class));
 
         }else {
